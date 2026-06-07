99e4431702ab69b261895cae4427f4103d5a0b81
itaro-san <italobelarmino58@gmail.com>
2026-06-07 14:32:29 +0000

    Adicionei a entrada e saída de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,31 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char Estado[50],Codigo[50],Cidade[50];
+  int População,Pontos;
+  float Area,PIB;
   // Área para entrada de dados
+ printf("Digite o Estado da sua carta:\n");
+ scanf("%s", Estado);
 
+ printf("Digite o código da sua carta:\n");
+ scanf("%s", Codigo);
+
+ printf("Digite o nome da cidade da sua carta:\n");
+ scanf("%s", Cidade);
+
+ printf("Digite a população da sua carta:\n");
+scanf("%d", &População);
+
+ printf("Digite os pontos da sua carta:\n");
+ scanf("%d", &Pontos);
+
+ printf("Digite a área da sua carta:\n");
+ scanf("%f", &Area);
+ 
+ printf("Digite a área da sua carta:\n");
+scanf("%f",&PIB);
   // Área para exibição dos dados da cidade
-
+printf()
 return 0;
 } 
