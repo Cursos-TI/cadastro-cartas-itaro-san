afa6c6c2b47ace2d46b59da0363dc69e2caf76cc
itaro-san <italobelarmino58@gmail.com>
2026-06-07 14:35:39 +0000

    Adicionei a entrada e saída de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,31 +6,39 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char Estado[50],Codigo[50],Cidade[50];
-  int População,Pontos;
-  float Area,PIB;
-  // Área para entrada de dados
- printf("Digite o Estado da sua carta:\n");
- scanf("%s", Estado);
+    char Estado[50],Codigo[50],Cidade[50];
+      int Populacao,Pontos;
+        float Area,PIB;
+          // Área para entrada de dados
+          printf("CADASTRE SUA CARTA\n");
+           printf("Digite o Estado da sua carta:\n");
+            scanf("%s", Estado);
 
- printf("Digite o código da sua carta:\n");
- scanf("%s", Codigo);
+             printf("Digite o código da sua carta:\n");
+              scanf("%s", Codigo);
 
- printf("Digite o nome da cidade da sua carta:\n");
- scanf("%s", Cidade);
+               printf("Digite o nome da cidade da sua carta:\n");
+                scanf("%s", Cidade);
 
- printf("Digite a população da sua carta:\n");
-scanf("%d", &População);
+                 printf("Digite a população da sua carta:\n");
+                 scanf("%d", &Populacao);
 
- printf("Digite os pontos da sua carta:\n");
- scanf("%d", &Pontos);
+                  printf("Digite os pontos da sua carta:\n");
+                   scanf("%d", &Pontos);
 
- printf("Digite a área da sua carta:\n");
- scanf("%f", &Area);
- 
- printf("Digite a área da sua carta:\n");
-scanf("%f",&PIB);
-  // Área para exibição dos dados da cidade
-printf()
-return 0;
-} 
+                    printf("Digite a área da sua carta:\n");
+                     scanf("%f", &Area);
+                      
+                       printf("Digite a PIB da sua carta:\n");
+                       scanf("%f",&PIB);
+                         // Área para exibição dos dados da cidade
+                         printf("CADASTRO COM SUCESSO\n");
+                         printf("Estado:%s\n", Estado);
+                         printf("Código:%s\n",Codigo);
+                         printf("Cidade:%s\n",Cidade);
+                         printf("População:%d\n",Populacao);
+                         printf("Pontos:%d\n",Pontos);
+                         printf("Área:%2.f\n",Area);
+                         printf("PIB:%2.f\n",PIB);
+                         return 0;
+                         } 
