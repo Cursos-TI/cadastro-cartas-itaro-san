9da8d87ac98c170e34ff23883e881b2e8348769d
itaro-san <italobelarmino58@gmail.com>
2026-06-12 00:56:12 +0000

    Adiciona comparação e exibição de resultados das cartas no desafio Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -22,7 +22,6 @@ int main()
     float Area2, PIB2, PIBper2, Densidade2;
     float SUPERPODER2;
 
-
     // Área para exibição do título do desafio
     printf("DESAFIO SUPER TRUNFO - PAÍSES\n");
     printf("TEMA 1 - CADASTRO DAS CARTAS\n");
@@ -89,6 +88,7 @@ int main()
     printf("\n===== DADOS DAS CARTAS CADASTRADAS COM SUCESSO =====\n");
 
     printf("===== CADASTRO COM SUCESSO DA CARTA 1 =====\n");
+
     printf("Estado:%s\n", Estado1);
     printf("Código:%s\n", Codigo1);
     printf("Cidade:%s\n", Cidade1);
@@ -110,5 +110,120 @@ int main()
     printf("PIB:%.2f bilhões de reais\n", PIB2);
     printf("Densidade Populacional:%.2f hab/km²\n", Densidade2);
     printf("PIB Per Capita:%.2f reais\n", PIBper2);
+
+    // Área de comparação das cartas e exibição do resultado do desafio
+    printf("\n===== RESULTADO DO DESAFIO SUPER TRUNFO - PAÍSES =====\n");
+    printf("Comparando as cartas cadastradas...\n");
+    printf("===== SUPER BATALHA DE CARTAS =====\n");
+
+    printf("Comparando a população das cartas...\n");
+
+    if (Populacao1 > Populacao2)
+    {
+        printf("A Carta 1 tem a maior população!\n");
+    }
+    else if (Populacao2 > Populacao1)
+    {
+        printf("A Carta 2 tem a maior população!\n");
+    }
+    else
+    {
+        printf("As duas cartas têm a mesma população!\n");
+    }
+
+    printf("Comparando os pontos turísticos das cartas...\n");
+
+    if (Pontos1 > Pontos2)
+    {
+        printf("A Carta 1 tem mais pontos turísticos!\n");
+    }
+    else if (Pontos2 > Pontos1)
+    {
+        printf("A Carta 2 tem mais pontos turísticos!\n");
+    }
+    else
+    {
+        printf("As duas cartas têm o mesmo número de pontos turísticos!\n");
+    }
+
+    printf("Comparando as áreas das cartas...\n");
+
+    if (Area1 > Area2)
+    {
+        printf("A Carta 1 tem a maior área!\n");
+    }
+    else if (Area2 > Area1)
+    {
+        printf("A Carta 2 tem a maior área!\n");
+    }
+    else
+    {
+        printf("As duas cartas têm a mesma área!\n");
+    }
+
+    printf("Comparando os PIBs das cartas...\n");
+
+    if (PIB1 > PIB2)
+    {
+        printf("A Carta 1 tem o maior PIB!\n");
+    }
+    else if (PIB2 > PIB1)
+    {
+        printf("A Carta 2 tem o maior PIB!\n");
+    }
+    else
+    {
+        printf("As duas cartas têm o mesmo PIB!\n");
+    }
+
+    printf("Comparando os PIBs per capita das cartas...\n");
+
+    if (PIBper1 > PIBper2)
+    {
+        printf("A Carta 1 tem o maior PIB per capita!\n");
+    }
+    else if (PIBper2 > PIBper1)
+    {
+        printf("A Carta 2 tem o maior PIB per capita!\n");
+    }
+    else
+    {
+        printf("As duas cartas têm o mesmo PIB per capita!\n");
+    }
+
+    printf("Comparando as densidades populacionais das cartas...\n");
+
+    if (Densidade1 < Densidade2)
+    {
+        printf("A Carta 1 tem a menor densidade populacional!\n");
+    }
+    else if (Densidade2 < Densidade1)
+    {
+        printf("A Carta 2 tem a menor densidade populacional!\n");
+    }
+    else
+    {
+        printf("As duas cartas têm a mesma densidade populacional!\n");
+    }
+
+    // Área para cálculo do super poder de cada carta
+    SUPERPODER1 = Populacao1 + Pontos1 + Area1 + PIB1 + PIBper1 + Densidade1;
+    SUPERPODER2 = Populacao2 + Pontos2 + Area2 + PIB2 + PIBper2 + Densidade2;
+
+    printf("===== RESULTADO DO DESAFIO =====\n");
+
+    if (SUPERPODER1 > SUPERPODER2)
+    {
+        printf("A Carta 1 é a vencedora com maior Super Poder !\n", SUPERPODER1);
+    }
+    else if (SUPERPODER2 > SUPERPODER1)
+    {
+        printf("A Carta 2 é a vencedora com maior Super Poder!\n", SUPERPODER2);
+    }
+    else
+    {
+        printf("As duas cartas têm o mesmo Super Poder!\n");
+    }
+
     return 0;
 }
